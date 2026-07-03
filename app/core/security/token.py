47fc5f6f04d7c66ee06a0550d4368d740c54e44f
XDKAY <sasha.naftaliev.02@mail.ru>
2026-07-03 14:46:50 +0300

    feat(auth): add JWT token generation and decoding utilities
    - Implement generate_token() for access and refresh tokens
    - Add decode_token() with expiration and validation error handling
    - Configure token settings from environment variables

---
+++ app/core/security/token.py
@@ -0,0 +1,61 @@
+import jwt
+
+from uuid import UUID
+from typing import Mapping
+from datetime import datetime, timezone, timedelta
+from fastapi import status, HTTPException
+
+from app.core.settings.settings import settings
+
+
+def generate_token(user_id: UUID, token_type: str) -> str:
+
+    """
+        token_type:
+            values: access/refresh
+    """
+    
+    match (token_type):
+        case "access":
+            expires = datetime.now(timezone.utc) + timedelta(minutes=settings.token.access_token_expires_minutes)
+
+        case "refresh":
+            expires = datetime.now(timezone.utc) + timedelta(days=settings.token.refresh_token_expires_days)
+
+        case _:
+            raise ValueError("Invalid token type")
+    
+    payload = {
+        "sub": str(user_id),
+        "type": token_type,
+        "exp": expires
+    }
+
+    return jwt.encode(
+        payload, 
+        settings.token.secret.get_secret_value(), 
+        algorithm=settings.token.algorithm
+    )
+
+
+def decode_token(token: str) -> Mapping[str, str]:
+    try:
+        payload = jwt.decode(
+            token,
+            settings.token.secret.get_secret_value(),
+            algorithms=[settings.token.algorithm]
+        )
+        payload["sub"] = UUID(payload.get("sub"))
+        return payload
+
+    except jwt.exceptions.ExpiredSignatureError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Token has expired"
+        )
+
+    except jwt.exceptions.InvalidTokenError:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid token"
+        )
