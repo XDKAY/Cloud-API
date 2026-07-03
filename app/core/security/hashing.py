a3f5c990ed3f9d2154b6b5d8c562999bd8225bf7
XDKAY <sasha.naftaliev.02@mail.ru>
2026-07-03 11:54:39 +0300

    feat: hashing the password, checking the hash

---
+++ app/core/security/hashing.py
@@ -0,0 +1,15 @@
+from passlib.context import CryptContext
+
+
+ctx = CryptContext(
+    schemes=["argon2"],
+    deprecated="auto"
+)
+
+
+def hash_password(password: str) -> str:
+    return ctx.hash(password)
+
+
+def verify_password(password: str, hashed_password: str) -> bool:
+    return ctx.verify(password, hashed_password)
